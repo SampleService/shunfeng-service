79d30f20b49cc03758ed35b69b251fb95cd23bef
SeptemberHX <september_hx@outlook.com>
2019-12-03 17:00:05 +0800

    f

--- src/main/java/com/sampleservice/shunfengservice/controller/MainController.java
+++ src/main/java/com/sampleservice/shunfengservice/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.sampleservice.shunfengservice.controller;
 
+import com.sampleservice.shunfengservice.utils.MBaseUtils;
 import com.septemberhx.common.base.MResponse;
 import com.septemberhx.mclient.annotation.MApiFunction;
 import com.septemberhx.mclient.annotation.MRestApiType;
@@ -20,6 +21,10 @@ public class MainController extends MObject {
     public MResponse payFunction(@RequestBody MResponse requestData) {
         MResponse result = new MResponse();
         result.set("msg", "/delivery");
+
+        if (requestData.get("interval") != null) {
+            MBaseUtils.generateStringInKBSize(2000, result, (int) requestData.get("interval"));
+        }
         return result;
     }
 }
